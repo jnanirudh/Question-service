4100e524cda4e1272e5fad623e8c5174ed351790
jnanirudh <jn.anirudh@gmail.com>
2026-01-30 15:19:44 +0530

    Added functionality to work with Quiz service

--- src/main/java/com/example/questionservice/controller/QuestionController.java
+++ src/main/java/com/example/questionservice/controller/QuestionController.java
@@ -1,10 +1,11 @@
 package com.example.questionservice.controller;
 
 import com.example.questionservice.model.Question;
-
+import com.example.questionservice.model.QuestionWrapper;
 import com.example.questionservice.service.QuestionService;
 //import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,20 +17,33 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
 
-    @GetMapping()
-    public List<Question> getAllQuestions(){
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
-//    @GetMapping("questionId")
-//    public List<Question> getSingleQuestion(PathParam String questionId)
-//    {
-//        return questionService.getSingleQuestion(questionId);
-//
-//    }
-
-    @PostMapping()
-    public String addQuestion(@RequestBody Question question){
+    // Add a new question to MySQL
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
+    }
+
+
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(
+            @RequestParam String subject, @RequestParam Integer numQ) {
+        return questionService.getQuestionsForQuiz(subject, numQ);
+    }
+
+// Get QuestionWrappers (DTOs) for the student view
+    @PostMapping("getQuestionsFromId")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds) {
+        return questionService.getQuestionsFromId(questionIds);
+    }
+
+// For Report Service - Calculate the final score
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses) {
+        return questionService.getScore(responses);
     }
 }
